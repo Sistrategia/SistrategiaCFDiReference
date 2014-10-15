b7dc63031b8b5ea0c4d59255865e39ee0e0cdcb0
José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
2014-10-15 12:32:13 -0500

    Sistrategia.SAT project files.

---
+++ src/Sistrategia.SAT/SATManager.cs
@@ -0,0 +1,38 @@
+﻿/**************************************************************************
+ * Sistrategia.SAT.CFDI.SATManager.cs 
+ * 
+ * Author(s):   José Ernesto Ocampo Cicero <ernesto@sistrategia.com>
+ * Last Update:	2014-Oct-10
+ * Created:	    2010-Dec-16
+ * 
+ * (C) 2010-2014 José Ernesto Ocampo Cicero
+ * Copyright (C) José Ernesto Ocampo Cicero, 2010-2014
+ * All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ *************************************************************************/
+
+using System;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using Sistrategia.SAT.CFDI;
+
+namespace Sistrategia.SAT
+{
+    public class SATManager
+    {
+        public static IComprobante CreateComprobante() {
+            return new Comprobante();
+        }   
+    }
+}
